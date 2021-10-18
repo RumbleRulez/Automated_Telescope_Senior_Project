5d407124a9ac475fa630966c728957601d19855a
George <georgeslagley@gmail.com>
2021-10-18 03:57:51 -0400

    added menu, and base vars

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,7 @@ vector<double> get_body(string body_name){
     file.close();
 }
 
+//print top level menu
 void print_top_menu(){
     cout << "Welcome to the automated telescope tracking interface. \nPlease press the number corresponding to the desired action:" << endl;
     cout << "1. Track a Celestial body by name" << endl;
@@ -47,6 +48,7 @@ void print_top_menu(){
 
     return;
 }
+//print menu for celestial body
 void print_body_menu(){
     cout << "1.Earth's moon" << endl;
 
@@ -57,10 +59,38 @@ void print_body_menu(){
 int main(int argc, char *argv[]){
    // NEED PINS FOR EASYDRIVER
    // EasyDriver::EasyDriver drive = new EasyDriver();
-    
+
+    //hold vars for menu
+    int top_choice;
+    bool isOn = true;
+    double azi, elev;
+    string body;
+    //declare sys start
     cout << "System Initialized" << endl;
-    print_body_menu();
-    print_top_menu();
+
+    //loop to keep alive
+    while(isOn == true){
+    
+        //print choice menu
+        print_top_menu();
+        
+        switch(top_choice){
+            case 0:
+                cout << "Thank you for using this product!" << endl;
+                isOn  = false;
+                break;
+            case 1:
+                print_body_menu();
+                break;
+            case 2:
+                
+                break;
+            default:
+                cout << "Invalid option, please input the number corresponding to the choice" << endl;
+                break;
+        }
+
+    }
 
     return 0;
 }
