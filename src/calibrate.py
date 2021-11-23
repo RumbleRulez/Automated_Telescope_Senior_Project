0a6b2ecea32b35ce9b8ce5b9da61d1dd6ac6a36b
user <georgeslagley@gmail.com>
2021-11-23 01:39:47 -0500

    plez

--- src/calibrate.py
+++ src/calibrate.py
@@ -1,10 +1,13 @@
-from os import write
+from os import wait, write
 import time
 import board
 import adafruit_bno055
 
 i2c = board.I2C()
 sensor = adafruit_bno055.BNO055_I2C(i2c)
-
-print("Calibrating")
-
+sensor.mode = adafruit_bno055.NDOF_MODE
+time.sleep(1)
+print("Calibration Status:")
+sys, gyro, acc ,mag = sensor.calibration_status
+print(sensor.calibrated)
+print("Sys: ", sys," Gyro: ", gyro," Acc: ", acc, " Mag: ",mag)
