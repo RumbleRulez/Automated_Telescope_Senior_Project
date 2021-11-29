48b10c0c3d070d363c9e8cdee5190bd8bfaa58e9
user <georgeslagley@gmail.com>
2021-11-29 05:59:38 -0500

    changed change pos to get perc diff

--- src/main.cpp
+++ src/main.cpp
@@ -173,15 +173,15 @@ void change_pos(int time, vector<vector<double>> future, vector<vector<double>> 
     temp_curr = getIMU();
     perc_diff_azi =  (future[time][0] - temp_curr[0][0])/(future[time][0] - temp_curr[0][0]) *100;
     while(perc_diff_azi != 10){
-        AZI_drive.rotate(17.06349206*(future[0] - temp_curr[0][0]));
+        AZI_drive.rotate(17.06349206*(future[0][0] - temp_curr[0][0]));
         temp_curr = getIMU();
         perc_diff_azi =  future[time][0] - temp_curr[0][0])/(future[time][0] - temp_curr[0][0]) *100;
     }
     if(future[time][0]!= temp_curr[0][0])
-        AZI_drive.rotate(17.06349206*(future[0] + temp_curr[0][0]));
+        AZI_drive.rotate(17.06349206*(future[time][0] + temp_curr[0][0]));
 
     if(future[time][1]!= temp_curr[0][1])
-        ALT_drive.rotate(8.928571428*(future[1] + temp_curr[0][1]));
+        ALT_drive.rotate(8.928571428*(future[time][1] + temp_curr[0][1]));
 
     // current[0][0] = current[0][0] + hold[0]; 
     // current[0][1] = current[0][1] + hold[1];
