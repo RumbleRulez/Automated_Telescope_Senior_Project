5c8bf440da8d2f962089bf11899a41b02f8945f7
Demo User <debian@beaglebone.localdomain>
2021-11-29 09:48:40 +0000

    ree

--- src/IMU_Script.py
+++ src/IMU_Script.py
@@ -11,8 +11,8 @@ sensor = adafruit_bno055.BNO055_I2C(i2c)
 sensor.mode = adafruit_bno055.CONFIG_MODE
 
 sensor.offsets_gyroscope = -1, -2, 0
-sensor.offsets_accelerometer = -25,-9,-21
-sensor.offsets_magnetometer = 40,31,-252
+sensor.offsets_accelerometer = -8,-28,-23
+sensor.offsets_magnetometer = 283,52,-824
 
 sensor.mode = adafruit_bno055.NDOF_MODE
 print("Writing Data...")
@@ -23,6 +23,6 @@ def writeData():
         csvwriter = csv.writer(csvfile)
         time.sleep(1)
         csvwriter.writerow(sensor.euler)
-        #print(sensor.euler)
+        print(sensor.euler)
 
 writeData()
